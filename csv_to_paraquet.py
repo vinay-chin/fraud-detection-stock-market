e44a1f46013520cc801a8f0a782dec88f8f38549
VINAY CHINDUKURI <chvinay8288@gmail.com>
2025-02-14 13:20:03 +0530

    pushed requirements.txt

--- csv_to_paraquet.py
+++ csv_to_paraquet.py
@@ -8,7 +8,7 @@ def csv_to_parquet(csv_file):
     table = pa.Table.from_pandas(df)
     parquet_file = os.path.splitext(csv_file)[0] + '.parquet'
     pq.write_table(table, parquet_file)
-
+ 
 def read_paraquet(parquet_file):
     table = pq.read_table(parquet_file)
     print(table.to_pandas())
